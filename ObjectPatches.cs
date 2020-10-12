3d3c66f690facf4c817230e04de1d46af1981f97
Freiling87 <lucas87t@gmail.com>
2020-10-12 09:06:44 -0500

    Cleared out errors

--- ObjectPatches.cs
+++ ObjectPatches.cs
@@ -1,39 +1,40 @@
-﻿using System;
+﻿using BepInEx;
+using HarmonyLib;
+using RogueLibsCore;
+using System;
+using System.Reflection;
+using UnityEngine;
+
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using BepInEx;
-using HarmonyLib;
-using UnityEngine;
-using RogueLibsCore;
-using System.Reflection;
 
 namespace BunnyMod
 {
-    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+	[BepInPlugin(pluginGuid, pluginName, pluginVersion)]
     [BepInProcess("StreetsOfRogue.exe")]
     [BepInDependency(RogueLibs.pluginGuid, "1.0")]
+
     public class ObjectPatches : BaseUnityPlugin
     {
         public const string pluginGuid = "freiling87.streetsofrogue.bunnymod";
         public const string pluginName = "Bunny Mod";
         public const string pluginVersion = "1.0.0.0";
 
-        private static object __instance;
-
-
         public void Awake()
 		{
-            this.PatchPrefix(typeof(Bathtub), "Interact", GetType(), "Interact_Patch");
+            Logger.LogInfo("BunnyMod loaded");
+            this.PatchPrefix(typeof(ObjectReal), "Interact", GetType(), "Interact_Patch");
         }
 
-        public static bool Interact_Patch(Agent agent)
+        public static bool Interact_Patch(Agent agent, Bathtub __instance)
         {
+            if (!(__instance is Bathtub b)) return true;
 
             MethodInfo baseMethod = AccessTools.DeclaredMethod(__instance.GetType().BaseType, "Interact");
             baseMethod.GetMethodWithoutOverrides<Action<Agent>>(__instance).Invoke(agent);
 
-            this.interactingAgent.statusEffects.BecomeHidden(this);
+            agent.statusEffects.BecomeHidden(__instance);
 
             baseMethod = AccessTools.DeclaredMethod(__instance.GetType().BaseType, "StopInteraction");
             baseMethod.GetMethodWithoutOverrides<Action>(__instance).Invoke();

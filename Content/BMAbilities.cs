91cf955c854041d0c40774a26aee9530cd99bbf0
FN LN <lucas87t@gmail.com>
2021-06-03 10:53:51 -0500

    Filled out Magic dialogue

--- Content/BMAbilities.cs
+++ Content/BMAbilities.cs
@@ -1,8 +1,10 @@
 ﻿using RogueLibsCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace BunnyMod.Content
 {
@@ -33,17 +35,29 @@ namespace BunnyMod.Content
 			t = vNameType.Dialogue;
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_CantDo1, t, new CustomNameInfo("I need to take a \"time out!\" Get it? But seriously, my heart will stop."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_CantDo2, t, new CustomNameInfo("I'm gonna take the blue pill for a sec."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_CantDo3, t, new CustomNameInfo("I think this is giving me dementia."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_CantDo4, t, new CustomNameInfo("Slow your roll! If I create a spacetime singularity people are gonna be mad."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Cast1, t, new CustomNameInfo("Stop right there! Okay, slowing down is cool too."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Cast2, t, new CustomNameInfo("Swallow this, clock-suckers!"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Cast3, t, new CustomNameInfo("Nothing can slow me down!"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Cast4, t, new CustomNameInfo("Freeze! In time, not in ice."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Miscast1, t, new CustomNameInfo("Iii ttthhhiiinnnkkk Iii mmmeeesssssseeeddd uuuppp..."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Miscast2, t, new CustomNameInfo("Bullet Time? More like Bullshit Time!"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Miscast3, t, new CustomNameInfo("(Slow Motion Noises)"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Miscast4, t, new CustomNameInfo("Okay, maybe there is a spoon, I guess. Whatever."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Recharge1, t, new CustomNameInfo("It's Slowing-down-time... time!"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Recharge2, t, new CustomNameInfo("Not many time puns left. Uh... clock-a-doodle-doo?"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Recharge3, t, new CustomNameInfo("All wound up and ready to run. Like a clock."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_CD_Recharge4, t, new CustomNameInfo("I've got a need for relative speed!"));
+
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_CantDo1, t, new CustomNameInfo("I'm burned out."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_CantDo2, t, new CustomNameInfo("I don't feel like exploding right now."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_CantDo3, t, new CustomNameInfo("I need to eat more beans!"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_CantDo4, t, new CustomNameInfo("Gimme a sec, I need to pop all these blisters from the heat."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Cast1, t, new CustomNameInfo("Die! Burn! Die! Die!"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Cast2, t, new CustomNameInfo("Burn, baby, burn!"));
-			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Cast3, t, new CustomNameInfo("BURN-ie 2024!"));
-			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Cast4, t, new CustomNameInfo("Yer fired! Get it?"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Cast3, t, new CustomNameInfo("BURN-ie would have won!"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Cast4, t, new CustomNameInfo("You're fired! Hahaha!"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Miscast1, t, new CustomNameInfo("Not very stoked right now."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Miscast2, t, new CustomNameInfo("Haha my skin is melting lol XDDD"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Miscast3, t, new CustomNameInfo("Flame off! Flame off!"));
@@ -52,15 +66,23 @@ namespace BunnyMod.Content
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Recharge2, t, new CustomNameInfo("I'm here to burn things and chew bubblegum. I'm not out of gum, but I'm still gonna do a lot of burning."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Recharge3, t, new CustomNameInfo("(Laughs maniacally)"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_PJ_Recharge4, t, new CustomNameInfo("Why are the innocent so fun to burn?"));
+
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_CantDo1, t, new CustomNameInfo("I need to give it a rest or my head will explode. I've seen it happen."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_CantDo2, t, new CustomNameInfo("Slow down! Haven't you seen The Fly?"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_CantDo3, t, new CustomNameInfo("Don't abuse Spacetime too much, or you'll blink out of existence."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_CantDo4, t, new CustomNameInfo("Let me stay here for a sec."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Cast1, t, new CustomNameInfo("Vwip!"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Cast2, t, new CustomNameInfo("Nothing personal, kid."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Cast3, t, new CustomNameInfo("If you blink, I blink. And I'm gone when you open your eyes."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Cast4, t, new CustomNameInfo("Man, some smoke grenades would make this a lot cooler."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Miscast1, t, new CustomNameInfo("I smell burning toast."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Miscast2, t, new CustomNameInfo("Blurgh (Drool)"));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Miscast3, t, new CustomNameInfo("I pink I bust hab a stwoke."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Miscast4, t, new CustomNameInfo("My head a splode."));
 			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Recharge1, t, new CustomNameInfo("Who needs Scotty? I'll beam my damn self up."));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Recharge2, t, new CustomNameInfo("All charged up and ready to blink!"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Recharge3, t, new CustomNameInfo("Where do you want me?"));
+			_ = RogueLibs.CreateCustomName(cDialogue.MSA_TB_Recharge4, t, new CustomNameInfo("Let's get outta here."));
 		}
 
 		#region Magic General
@@ -132,13 +154,16 @@ namespace BunnyMod.Content
 		{
 			GC.audioHandler.Play(agent, "CantDo");
 
-			string[] dialogue =
+			List<string> dialogue = new List<string>()
 			{
 				cDialogue.MSA_CD_CantDo1,
-				cDialogue.MSA_CD_CantDo2
+				cDialogue.MSA_CD_CantDo2,
+				cDialogue.MSA_CD_CantDo3,
+				cDialogue.MSA_CD_CantDo4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_CD_DialogueCast(Agent agent) // TODO
 		{
@@ -146,9 +171,12 @@ namespace BunnyMod.Content
 			{
 				cDialogue.MSA_CD_Cast1,
 				cDialogue.MSA_CD_Cast2,
+				cDialogue.MSA_CD_Cast3,
+				cDialogue.MSA_CD_Cast4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_CD_DialogueMiscast(Agent agent)
 		{
@@ -160,16 +188,21 @@ namespace BunnyMod.Content
 				cDialogue.MSA_CD_Miscast4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_CD_DialogueRecharge(Agent agent)
 		{
 			string[] dialogue =
 			{
-				cDialogue.MSA_CD_Recharge1
+				cDialogue.MSA_CD_Recharge1,
+				cDialogue.MSA_CD_Recharge2,
+				cDialogue.MSA_CD_Recharge3,
+				cDialogue.MSA_CD_Recharge4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		#endregion
 		public static float baseTimeScale;
@@ -489,10 +522,13 @@ namespace BunnyMod.Content
 			string[] dialogue =
 			{
 				cDialogue.MSA_PJ_CantDo1,
-				cDialogue.MSA_PJ_CantDo2
+				cDialogue.MSA_PJ_CantDo2,
+				cDialogue.MSA_PJ_CantDo3,
+				cDialogue.MSA_PJ_CantDo4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_PJ_DialogueCast(Agent agent) // Not used yet
 		{
@@ -503,10 +539,11 @@ namespace BunnyMod.Content
 				cDialogue.MSA_PJ_Cast1,
 				cDialogue.MSA_PJ_Cast2,
 				cDialogue.MSA_PJ_Cast3,
-				cDialogue.MSA_PJ_Cast4
+				cDialogue.MSA_PJ_Cast4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_PJ_DialogueMiscast(Agent agent)
 		{
@@ -518,7 +555,8 @@ namespace BunnyMod.Content
 				cDialogue.MSA_PJ_Miscast4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_PJ_DialogueRecharge(Agent agent)
 		{
@@ -530,7 +568,8 @@ namespace BunnyMod.Content
 				cDialogue.MSA_PJ_Recharge4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		#endregion
 		public static void MSA_PJ_Initialize()
@@ -827,22 +866,19 @@ namespace BunnyMod.Content
 		{
 			GC.audioHandler.Play(agent, "CantDo");
 
-			if (GC.percentChance(75))
-				return;
-
 			string[] dialogue =
 			{
 				cDialogue.MSA_TB_CantDo1,
 				cDialogue.MSA_TB_CantDo2,
+				cDialogue.MSA_TB_CantDo3,
+				cDialogue.MSA_TB_CantDo4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_TB_DialogueCast(Agent agent)
 		{
-			if (GC.percentChance(75))
-				return;
-
 			agent.SpawnParticleEffect("Spawn", agent.curPosition);
 			GameController.gameController.audioHandler.Play(agent, "Spawn");
 
@@ -850,9 +886,12 @@ namespace BunnyMod.Content
 			{
 				cDialogue.MSA_TB_Cast1,
 				cDialogue.MSA_TB_Cast2,
+				cDialogue.MSA_TB_Cast3,
+				cDialogue.MSA_TB_Cast4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_TB_DialogueFullyCharged(Agent agent) // TODO
 		{
@@ -871,16 +910,21 @@ namespace BunnyMod.Content
 				cDialogue.MSA_TB_Miscast4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		public static void MSA_TB_DialogueRecharge(Agent agent)
 		{
 			string[] dialogue =
 			{
-				cDialogue.MSA_TB_Recharge1
+				cDialogue.MSA_TB_Recharge1,
+				cDialogue.MSA_TB_Recharge2,
+				cDialogue.MSA_TB_Recharge3,
+				cDialogue.MSA_TB_Recharge4,
 			};
 
-			BMHeaderTools.SayDialogue(agent, dialogue[UnityEngine.Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
+			if (GC.percentChance(25))
+				BMHeaderTools.SayDialogue(agent, dialogue[Random.Range(0, dialogue.Count() - 1)], vNameType.Dialogue);
 		}
 		#endregion
 		public static void MSA_TB_Initialize()

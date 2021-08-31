3b5d7f97d6a2ea5c9b99a24401de185db8e4a25e
BlazingTwist <m.h.Freedom2@web.de>
2021-08-31 22:38:40 +0200

    more extension methods

---
+++ Content/Extensions/StatusEffects_Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Google2u;
+
+namespace BunnyMod.Content.Extensions
+{
+	public static class StatusEffects_Extensions
+	{
+		public static void AddStatusEffect(this StatusEffects statusEffects, StatusEffectNameDB.rowIds statusEffect)
+		{
+			statusEffects.AddStatusEffect(statusEffect.GetName());
+		}
+
+		public static void AddStatusEffect(this StatusEffects statusEffects, StatusEffectNameDB.rowIds statusEffect, bool showText, bool dontPrevent)
+		{
+			statusEffects.AddStatusEffect(statusEffect.GetName(), showText, dontPrevent);
+		}
+	}
+}

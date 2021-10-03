a2a617a197c7336b7bfaa42dadb9d0656adb66ba
BlazingTwist <m.h.Freedom2@web.de>
2021-10-03 19:28:49 +0200

    BMObjects mostly converted
    committing early for merge

---
+++ Content/Patches/Objects/Plant_Patches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HarmonyLib;
+
+namespace BunnyMod.Content.Patches
+{
+	[HarmonyPatch(declaringType: typeof(Plant))]
+	public static class Plant_Patches
+	{
+		[HarmonyPostfix, HarmonyPatch(methodName: nameof(Plant.SetVars), argumentTypes: new Type[] { })]
+		private static void SetVars_Postfix(Plant __instance)
+		{
+			__instance.interactable = true;
+		}
+	}
+}
